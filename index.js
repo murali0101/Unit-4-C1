39dbb76e87ea1b3e609d1007f2ff1a3eaead1490
murali0101 <muralimv0101@gmail.com>
2022-03-07 12:36:37 +0530

    U4-C1

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+app.use(logger)
+function logger(req, res, next) {
+  console.log(req.path);
+  next();
+}
+function checkPermission(val) {
+  return function (req, res, next) {
+    if ((req.path = `\${val}`)) {
+      req.bag = { route: req.path, permission: true };
+      next();
+    }
+  };
+}
+
+app.get("/books",(req, res) => {
+  res.send({ route: "/books" });
+});
+
+app.get("/libraries",checkPermission("libraries"), (req, res) => {
+  res.send(req.bag);
+});
+app.get("/authors",checkPermission("authors"), (req, res) => {
+  res.send(req.bag);
+});
+
+app.listen(5500, () => {
+  console.log(5500);
+});
